fd1490e84d4b8eafe73cf45d6adadfdc157ecf4b
Kovacs Arthur <kovacs.arthur.kraid@gmail.com>
2014-01-08 03:25:02 +0200

    Working on the detail page.

--- src/app/section/detail/detail.js
+++ src/app/section/detail/detail.js
@@ -15,7 +15,7 @@ angular.module( 'tivity.detail', [
       console.log(data[0].response.venue);
       $scope.venue = data[0].response.venue;
       $scope.image = data[0].response.venue.photos.groups[0].items[0].prefix + 'width' + $window.innerWidth + data[0].response.venue.photos.groups[0].items[0].suffix;
-
+      $scope.icon =  data[0].response.venue.categories[0].icon.prefix + '88' + data[0].response.venue.categories[0].icon.suffix;
     });
   })
 
