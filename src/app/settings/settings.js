9ea079a326db5c7c5e63dcdefdb669f740d2b1c9
Kovacs Arthur <kovacs.arthur.kraid@gmail.com>
2014-01-16 22:34:28 +0200

    Made MongoLabs MongoDB Integration. Close #38

--- src/app/settings/settings.js
+++ src/app/settings/settings.js
@@ -2,6 +2,7 @@
 angular.module( 'tivity.settings', [
     'ui.router.state',
     'foursquare',
+    'storageManagement',
     //'geolocation', TODO: Enable when doing gMaps Routes
     'ajoslin.promise-tracker'/*,
     'google-maps',
@@ -27,7 +28,35 @@ angular.module( 'tivity.settings', [
       data:{ pageTitle: 'Settings' }
     });
   })
-  .controller( 'SettingsCtrl', function SettinsgController( $scope, geolocation, foursquare, $stateParams, $window, promiseTracker, $location, $anchorScroll ) {
+  .controller( 'SettingsCtrl', function SettinsgController( $scope, storageManagement ) {
+
+    //This method creates a new collection in the MongoDB, we don't need it anymore, we have the three necessary collections already made.
+    // Collections: favorites, excluded, switches
+    /*var theObject = {};
+    storageManagement.createCollection('switches', theObject).then(function(data){
+      //console.log(data);
+    });*/
+
+    //editCollectionID parameters: collectionID(collection name), collectionData(the JSON Object), dataID (the data ID inside the collection)
+    var theObject = {venue: "Old House"};
+    var dataID = '52d83a61e4b00bec814de847';
+    storageManagement.addEditCollectionID('excluded', theObject, dataID).then(function(data){
+      console.log('addEditCollectionID METHOD CALL results:');
+      console.log(data);
+    });
+    storageManagement.viewCollectionID('excluded', dataID).then(function(data){
+      console.log('viewCollectionID METHOD CALL results:');
+      console.log(data);
+    });
+
+    storageManagement.listCollections().then(function(data){
+      console.log(data);
+    });
+    storageManagement.listCollection('excluded').then(function(data){
+      console.log('listCollection METHOD CALL results:');
+      console.log(data);
+    });
+
 
   })
 ;
---
+++ src/common/services/storageManagement/storageManagement.js
@@ -0,0 +1,80 @@
+angular.module('storageManagement', [])
+  .factory('storageManagement', function ($http, $q) {
+    return {
+      listCollections: function () {
+        return $q.all([
+            $http.get('https://api.mongolab.com/api/1/databases/tivity/collections?apiKey=SN7DF704FBSTjFq5rycwwMHeBluJK4dT',
+              {
+                tracker: 'loadingTracker'
+              })
+          ]).then(function (results) {
+            var aggregatedData = [];
+            angular.forEach(results, function (result) {
+              aggregatedData = aggregatedData.concat(result.data);
+            });
+            return aggregatedData;
+          });
+      },
+      listCollection: function (collectionID) {
+        return $q.all([
+            $http.get('https://api.mongolab.com/api/1/databases/tivity/collections/' + collectionID + '?apiKey=SN7DF704FBSTjFq5rycwwMHeBluJK4dT',
+              {
+                tracker: 'loadingTracker'
+              })
+          ]).then(function (results) {
+            var aggregatedData = [];
+            angular.forEach(results, function (result) {
+              aggregatedData = aggregatedData.concat(result.data);
+            });
+            return aggregatedData;
+          });
+      },
+      createCollection: function (collectionID, collectionData) {
+        return $q.all([
+            $http.post(
+              'https://api.mongolab.com/api/1/databases/tivity/collections/' + collectionID + '?apiKey=SN7DF704FBSTjFq5rycwwMHeBluJK4dT',
+              collectionData
+            )
+          ]).then(function (results) {
+            console.log(results);
+            var aggregatedData = [];
+            angular.forEach(results, function (result) {
+              aggregatedData = aggregatedData.concat(result.data);
+            });
+            return aggregatedData;
+          });
+      },
+      addEditCollectionID: function (collectionID, collectionData, dataID) {
+        return $q.all([
+            $http.put(
+              'https://api.mongolab.com/api/1/databases/tivity/collections/' + collectionID + '/' + dataID  + '?apiKey=SN7DF704FBSTjFq5rycwwMHeBluJK4dT',
+              collectionData
+            )
+          ]).then(function (results) {
+            console.log('addEditCollectionID METHOD results:');
+            console.log(results);
+            var aggregatedData = [];
+            angular.forEach(results, function (result) {
+              aggregatedData = aggregatedData.concat(result.data);
+            });
+            return aggregatedData;
+          });
+      },
+      viewCollectionID: function (collectionID, dataID) {
+        return $q.all([
+            $http.get(
+              'https://api.mongolab.com/api/1/databases/tivity/collections/' + collectionID + '/' + dataID  + '?apiKey=SN7DF704FBSTjFq5rycwwMHeBluJK4dT'
+            )
+          ]).then(function (results) {
+            console.log('viewCollectionID METHOD results:');
+            console.log(results);
+            var aggregatedData = [];
+            angular.forEach(results, function (result) {
+              aggregatedData = aggregatedData.concat(result.data);
+            });
+            return aggregatedData;
+          });
+      }
+
+    };
+  });
