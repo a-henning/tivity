b4bfe4681ff944e37a5fee972026b3f1c5d339a1
Kovacs Arthur <kovacs.arthur.kraid@gmail.com>
2014-01-16 17:12:43 +0200

    Small fix to workaround the gmaps api js loading problem. Not Final. Fix #35.

--- src/app/app.js
+++ src/app/app.js
@@ -36,6 +36,14 @@ angular.module('tivity', [
       };
       console.log(currentState.name);
     });
+    gMaps = function() {
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+        'callback=initialize';
+      document.body.appendChild(script);
+    };
+    window.onload = gMaps();
   })
 
   .controller( 'AppCtrl', function AppCtrl ( $scope, $location, $stateParams ) {
