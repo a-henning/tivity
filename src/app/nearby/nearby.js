33e5735b2766a45e3cbfa199d96a7a43a20bb872
Arthur Henning <h_arthur_12@yahoo.com>
2014-03-03 17:12:42 +0200

    Implemented nearby places listing with static values.

--- src/app/nearby/nearby.js
+++ src/app/nearby/nearby.js
@@ -3,7 +3,7 @@
  */
 angular.module( 'tivity.nearby', [
         'ui.router.state',
-        'firebase'
+        'foursquare'
     ])
 
     .config(function config( $stateProvider ) {
@@ -15,12 +15,19 @@ angular.module( 'tivity.nearby', [
                     templateUrl: 'nearby/nearby.tpl.html'
                 }
             },
-            data:{ pageTitle: 'Nearby places?' }
+            data:{ pageTitle: 'Nearby places' }
         });
     })
 
-    .controller( 'NearbyCtrl', function AboutCtrl( $scope ) {
-        $scope.message = "Places nearby to you";
-    })
+    .controller( 'NearbyCtrl', function NearbyCtrl( $scope, foursquare ) {
+        $scope.near = "Cluj-Napoca";
+        $scope.radius = 10;
+        //With the location at hand, we're calling the foursquare service.
+        foursquare.getNearby($scope.near, $scope.radius).then(function(data){
 
+            //when the data is ready, populate the $scope variables.
+            $scope.locations = data[0].response.groups[0].items;
+            $scope.locationName = data[0].response.headerFullLocation;
+        });
+    })
 ;
